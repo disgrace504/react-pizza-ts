dbb59a5ae126abb2aed73b8c291a73e9a99b3122
disgrace504 <disgrace504@mail.ru>
2024-08-13 00:38:23 +0300

    Сделал скелет пиццы html+scss скачал у арчакова

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-function App() {
+import './assets/app.scss'
+import { Categories } from './components/Categories'
+import { Header } from './components/Header'
+import { Pizza } from './components/Pizza'
+import { Sort } from './components/Sort'
+
+export const App = () => {
   return (
     <>
-      <div>REACT PIZZA</div>
+      <div className='wrapper'>
+        <Header />
+        <div className='content'>
+          <div className='container'>
+            <div className='content__top'>
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className='content__title'>Все пиццы</h2>
+            <div className='content__items'>
+              <Pizza title='Чизбургер-пицца' price={395} />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
-
-export default App
